e9d0151590d531aed34196cafdede364ae86c114
Posa Levente <h376512@stud.u-szeged.hu>
2026-03-25 19:59:56 +0100

    lang skeleton 0.1

---
+++ lang/en/mod_cykalgorithm.php
@@ -0,0 +1,23 @@
+<?php
+
+$string['pluginname'] = 'CYK algorithm practice';
+$string['modulename'] = 'CYK algorithm practice';
+$string['modulenameplural'] = 'CYK algorithm practices';
+$string['pluginadministration'] = 'CYK algorithm administration';
+
+$string['cykalgorithmname'] = 'Activity name';
+$string['cykalgorithmname_help'] = 'Name of this CYK practice activity.';
+
+$string['grammar'] = 'Grammar (CNF)';
+$string['grammar_help'] = 'Specify the grammar in a simple text format, e.g.:
+S -> AS\' | AB
+S\' -> SB
+A -> a
+B -> b';
+
+$string['inputstring'] = 'Input word';
+$string['inputstring_help'] = 'The word the student must parse with the CYK algorithm.';
+
+$string['attemptresult'] = 'Your CYK table';
+$string['correct'] = 'Correct';
+$string['incorrect'] = 'Incorrect';
